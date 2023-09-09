54d2ac599ce77b9d7455f5b108395d9cd50027f2
Jennifer Patton <jenbuckleypatton@gmail.com>
2023-09-08 18:35:47 -0700

    making a middleware and api router

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-// Setup your Middleware and API Router here
+// Middleware
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
+
+// API Router
+const activitiesRouter = require("./routes/activities");
+const routinesRouter = require("./routes/routines");
+
+// Use the API routers
+app.use("/api/activities", activitiesRouter);
+app.use("/api/routines", routinesRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 module.exports = app;
+
